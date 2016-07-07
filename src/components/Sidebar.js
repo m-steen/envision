44f1819cb4abcc6c1618096eb642476a599de347
bart <bart@bartmolenkamp.nl>
2016-07-07 15:53:00 +0200

    Added buttons to change the color

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,13 +9,26 @@ class Sidebar extends Component {
             ? <div className="sidebar sidebar-open">
                 <input onChange={this.onChange} value={selection.title} />
                 X: {selection.x}, Y: {selection.y}
+                <div>
+                  <a onClick={(e) => this.onChangeColor("yellow")} className="color-button" style={{backgroundColor: "yellow"}}></a>
+                  <a onClick={(e) => this.onChangeColor("blue")} className="color-button" style={{backgroundColor: "blue"}}></a>
+                  <a onClick={(e) => this.onChangeColor("red")} className="color-button" style={{backgroundColor: "red"}}></a>
+                  <a onClick={(e) => this.onChangeColor("green")} className="color-button" style={{backgroundColor: "green"}}></a>
+                  <a onClick={(e) => this.onChangeColor("white")} className="color-button" style={{backgroundColor: "white"}}></a>
+                  <a onClick={(e) => this.onChangeColor("orange")} className="color-button" style={{backgroundColor: "orange"}}></a>
+                </div>
               </div>
             : <div className="sidebar" />;
     }
 
     onChange = (e) => {
         this.props.store.selection.title = e.target.value;
-        console.log(this.props.store.selection.title);
+        console.log("Set title to: " + this.props.store.selection.title);
+    }
+
+    onChangeColor(color) {
+      this.props.store.selection.color = color;
+      console.log("Set color to: " + color);
     }
 }
 
