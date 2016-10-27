4a81ead2b5cc37afbe59a962a33472da7ebb5d00
bart <bart@bartmolenkamp.nl>
2016-10-27 19:43:34 +0200

    Cleanup of code

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,35 +4,12 @@ import { render } from 'react-dom';
 import store from './AppState';
 import App from './components/App';
 
-import bmcPostIt from './model/bmcPostIt';
 import {newBmcModel, loadModels} from './commands';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
-
-function reload() {
-  fetch('http://webtoolstudio.bizzdesign.com:3001/models/0')
-  .then(function(response) {
-    return response.json();
-  }).then(function(json) {
-    store.model = json;
-  }).catch(function(ex) {
-    console.log('parsing failed', ex);
-  })
-};
-
-function save() {
-  const json = JSON.stringify(store.model);
-  fetch('http://webtoolstudio.bizzdesign.com:3001/models/0', {
-  	method: 'put',
-    headers: new Headers({
-  		'Content-Type': 'application/json'
-  	}),
-    body: json
-  });
-};
 
 function exportToJson() {
   const json = JSON.stringify(store.model);
