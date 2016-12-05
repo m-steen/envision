684c10037fa572895af81e10d000fd4e17fd27c8
Bart Molenkamp <b.molenkamp@bizzdesign.com>
2016-12-05 12:52:18 +0100

    Disable authentication used for debug

--- client/src/AppState.js
+++ client/src/AppState.js
@@ -17,8 +17,8 @@ function authenticationInfo() {
   var userId = readCookie("userId");
   var secret = readCookie("secret");
 
-  userId = userId || "691f9cd6dd826701x6a797679x15869400418x-371f";
-  secret = secret || "728b0c53-9554-493a-b414-0f8e9787e713";
+  //userId = userId || "691f9cd6dd826701x6a797679x15869400418x-371f";
+  //secret = secret || "728b0c53-9554-493a-b414-0f8e9787e713";
 
   if (userId && secret) {
     return {
